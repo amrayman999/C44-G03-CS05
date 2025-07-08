10904747781e2df4a801a20a969de8906b167274
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-07-08 18:27:13 +0300

    Question 2

--- Assignment/Program.cs
+++ Assignment/Program.cs
@@ -5,21 +5,35 @@
         static void Main(string[] args)
         {
             #region Question 1
-            Console.Write("enter Size of the MAtrix: ");
+            //Console.Write("enter Size of the MAtrix: ");
+            //int size = int.Parse(Console.ReadLine());
+            //int[,] matrix = new int[size,size];
+            //for (int i = 0; i < size; i++)
+            //{
+            //    for(int j = 0; j < size; j++)
+            //    {
+            //        if(i == j)
+            //           matrix[i,j] = 1;
+            //        else if(j != i)
+            //            matrix[i,j] = 0;
+            //        Console.Write($"{matrix[i,j]}  ");
+            //    }
+            //    Console.WriteLine();
+            //}
+            #endregion
+
+            #region Question 2
+            Console.Write("Enter size of array: ");
             int size = int.Parse(Console.ReadLine());
-            int[,] matrix = new int[size,size];
-            for (int i = 0; i < size; i++)
+            int[] arr = new int[size];
+            int sum = 0;
+            for(int i = 0; i < size; i++)
             {
-                for(int j = 0; j < size; j++)
-                {
-                    if(i == j)
-                       matrix[i,j] = 1;
-                    else if(j != i)
-                        matrix[i,j] = 0;
-                    Console.Write($"{matrix[i,j]}  ");
-                }
-                Console.WriteLine();
+                Console.Write($"Enter item number {i + 1} : ");
+                arr[i] = int.Parse(Console.ReadLine());
+                sum += arr[i];
             }
+            Console.Write($"Sum of Array items = {sum}");
             #endregion
         }
     }
