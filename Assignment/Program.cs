10cf6f9bee8bf498dc7e5dad5737dd36a93398a3
amrayman1999 <amr.ayman.essawi.1999@gmail.com>
2025-07-08 18:23:14 +0300

    Question 1

---
+++ Assignment/Program.cs
@@ -0,0 +1,26 @@
+﻿namespace Assignment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region Question 1
+            Console.Write("enter Size of the MAtrix: ");
+            int size = int.Parse(Console.ReadLine());
+            int[,] matrix = new int[size,size];
+            for (int i = 0; i < size; i++)
+            {
+                for(int j = 0; j < size; j++)
+                {
+                    if(i == j)
+                       matrix[i,j] = 1;
+                    else if(j != i)
+                        matrix[i,j] = 0;
+                    Console.Write($"{matrix[i,j]}  ");
+                }
+                Console.WriteLine();
+            }
+            #endregion
+        }
+    }
+}
